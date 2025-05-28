0ef6835e3908b9a0666a5e64680dce684c6968b0
Praful Tandekar <prafultandekar1997@gmail.com>
2025-05-28 08:46:45 +0530

    backendurl

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://notes-backend-v2.onrender.com/api',
 });
 
 API.interceptors.request.use((req) => {
